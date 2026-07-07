7e49ef0e5e046456f26dfd72f77bfcefa04ef67d
JackKirbyCook82 <jackcookchee@yahoo.com>
2026-07-07 04:14:56 -0500

    Updated 1/10/2024

--- __init__.py
+++ __init__.py
@@ -6,8 +6,8 @@ Created on Sun Jul 5 2026
 
 """
 
+import numpy as np
 import pandas as pd
-from types import NoneType
 
 from finance.variables import Enumerations
 from finance.logging import Logging
@@ -21,27 +21,19 @@ __license__ = "MIT License"
 
 
 class StockCalculator(Logging, Equations):
+    quality = lambda activity, tightness: activity / (tightness ** 2 + 1e-6)
+    activity = lambda supply, demand: np.minimum(supply, demand) / (np.maximum(supply, demand) + 10)
+    tightness = lambda gap, median: gap / median
     mean = lambda bid, ask, supply, demand: ((bid * demand) + (ask * supply)) / (demand + supply)
     median = lambda bid, ask: (bid + ask) / 2
     gap = lambda bid, ask: ask - bid
 
-    def __call__(self, stocks, technicals=None, /, **kwargs):
+    def __call__(self, stocks, /, **kwargs):
         assert isinstance(stocks, pd.DataFrame)
-        assert isinstance(technicals, (pd.DataFrame, NoneType))
-        if technicals is not None: stocks = self.merge(stocks, technicals, **kwargs)
         calculated = self.execute(stocks, **kwargs)
         stocks = pd.concat([stocks, calculated], axis=1)
         self.results(stocks, title="Calculated", instrument=Enumerations.Instrument.STOCK)
         return stocks
-
-    @staticmethod
-    def merge(stocks, technicals, /, **kwargs):
-        technicals = technicals[technicals["date"] <= pd.Timestamp.today()]
-        technicals = technicals.sort_values(["ticker", "date"]).groupby("ticker", as_index=False).last()
-        technicals = technicals[["ticker", "date", "volatility", "trend"]]
-        stocks = stocks.merge(technicals, on="ticker", how="left")
-        return stocks
-
 
 
 
